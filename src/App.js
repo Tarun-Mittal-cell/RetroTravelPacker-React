af88a69c25fab21263668e383ec4b8518b93ccba
Tarun Mittal <tm6622@g.rit.edu>
2023-06-22 19:45:22 -0400

    added test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 1, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -20,6 +21,8 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
   }
@@ -34,7 +37,7 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input type="text" placeholder="Item..." value={description} />
       <button>Add</button>
     </form>
   );
